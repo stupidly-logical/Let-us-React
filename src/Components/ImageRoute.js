29f9c863d73d9cf65def8cf066a8670081224f85
patelsatyam <patel.satyam147@gmail.com>
2018-09-10 23:41:56 +0530

    initial

--- src/Components/ImageRoute.js
+++ src/Components/ImageRoute.js
@@ -5,7 +5,7 @@ import '../App.css';
 class ImageRoute extends React.Component {
     constructor(props) {
         super(props);
-        this.state = ({ posts: [] })
+        this.state = ({ posts: [] });
     }
 
 
@@ -27,7 +27,6 @@ class ImageRoute extends React.Component {
                         <img src = {post.imageUrl}  alt = "ImageUrl" className = "App-image"/>
                      </div>     
                 ))}
-            
             </div>
         )
     }
